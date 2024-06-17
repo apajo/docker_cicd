764c5341a8ad7a222c9a38141d03a6a51a2d1e63
Andres Pajo <andres@apajo.ee>
2024-06-17 18:45:34 +0300

    Improved tests

--- .docker/test/scripts/start.sh
+++ .docker/test/scripts/start.sh
@@ -4,12 +4,12 @@ echo -e "\nReady to run the tests ... \n"
 
 wait-for-it.sh ${STAGING_HOST}:${STAGING_PORT} -t 10;
 
-echo "Deploying ..."
+echo "Testing / building ..."
 
 bin/ssh/staging stage test_repo 12345;
 
 wait-for-it.sh ${PRODUCTION_HOST}:${PRODUCTION_PORT} -t 10;
 
-echo "Setting up prod env ..."
+echo "Setting up built image ..."
 
-bin/ssh/prod deploy test_repo prod_12345;
+bin/ssh/prod deploy test_repo 12345;
