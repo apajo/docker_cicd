725c032b148f8df132d37c168c2ccee5b01a9613
Andres Pajo <andres@apajo.ee>
2024-06-17 20:56:21 +0300

    Fixed minor errors/warnings

--- .docker/test/scripts/start.sh
+++ .docker/test/scripts/start.sh
@@ -2,14 +2,13 @@
 
 echo -e "\nReady to run the tests ... \n"
 
-wait-for-it.sh ${STAGING_HOST}:${STAGING_PORT} -t 10;
+# Generate a random number for the version variable
+version=$RANDOM
 
 echo "Testing / building ..."
 
-bin/ssh/staging stage test_repo 12345;
-
-wait-for-it.sh ${PRODUCTION_HOST}:${PRODUCTION_PORT} -t 10;
+bin/ssh/staging stage test_repo $version;
 
 echo "Setting up built image ..."
 
-bin/ssh/prod deploy test_repo 12345;
+bin/ssh/prod deploy test_repo $version;
