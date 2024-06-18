b0df72fc6468a83ccd20914e4c7066e96814be13
Andres Pajo <andres@apajo.ee>
2024-06-18 04:06:28 +0300

    Removed test repo delays/sleeps

--- .docker/app/scripts/entrypoint.sh
+++ .docker/app/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo -e "\nApp is running ... \n"
 
-sleep 3
+sleep 1
 
 echo -e "\Environment variable VERSION: $VERSION \n"
 echo -e "\Environment variable SOME_ENV_VALUE: $SOME_ENV_VALUE \n"
@@ -10,7 +10,5 @@ echo -e "\Environment variable SOME_ENV_VALUE: $SOME_ENV_VALUE \n"
 sleep 1
 
 echo -e "\nApp is exiting ... \n"
-
-sleep 2
 
 exit 0;
