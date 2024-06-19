767a46f0076f04549e683ffc8f55400256b6c851
Andres Pajo <andres@apajo.ee>
2024-06-20 01:09:00 +0300

    Improved registry cleanup

--- .docker/registry/scripts/entrypoint.sh
+++ .docker/registry/scripts/entrypoint.sh
@@ -3,13 +3,11 @@
 # Custom initialization commands can go here
 echo "Starting registry..."
 
-# registry.py -l user:pass -r http://localhost:5000 --delete
+# Cleanup registry
+(sleep 10 && cleanup_registry ) &
 
-# Delete images (except last n) in 10 seconds
-(sleep 10 && (echo "Deleting images (except 10 last ones)..." && registry.py -r http://localhost:5000 --delete -n 10)) &
-
-# Reboot in 10 hours
-(sleep 36000 && reboot) &
+# Reboot in 1 hour
+(sleep 3600 && reboot) &
 
 # Execute the original entrypoint
 exec /entrypoint.sh "$@"
