c624742d91f9fed318e80e48bc36c711c0b5c6ff
Andres Pajo <andres@apajo.ee>
2024-06-23 01:21:58 +0300

    Test DNS lookup

--- .docker/app/scripts/entrypoint.sh
+++ .docker/app/scripts/entrypoint.sh
@@ -4,11 +4,20 @@ echo -e "\nApp is running ... \n"
 
 sleep 1
 
-echo -e "\Environment variable VERSION: $VERSION \n"
-echo -e "\Environment variable SOME_ENV_VALUE: $SOME_ENV_VALUE \n"
+echo -e "\nEnvironment variable VERSION: $VERSION \n"
+echo -e "\nEnvironment variable SOME_ENV_VALUE: $SOME_ENV_VALUE \n"
 
 sleep 1
 
+echo -e "\nChecking DNS lookup ... \n"
+
+nslookup github.com
+
+if [ $? -ne 0 ]; then
+  echo -e "DNS lookup failed!"
+  exit 1
+fi
+
 echo -e "\nApp is exiting ... \n"
 
-exit 0;
+#exit 0;
