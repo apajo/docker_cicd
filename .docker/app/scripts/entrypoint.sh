1523bb9bb3a10f8b95237a7e21aa36056e01a0a7
Andres Pajo <andres@apajo.ee>
2024-07-18 07:04:16 +0300

    Added TCP socker test and a volume to test repository

--- .docker/app/scripts/entrypoint.sh
+++ .docker/app/scripts/entrypoint.sh
@@ -4,6 +4,25 @@ echo -e "\nApp is running ... \n"
 
 sleep 1
 
+echo -e "\nList files in volume volume /app/volume ... \n"
+ls -la /app/volume
+
+sleep 1
+
+echo -e "\nTest TCP server/socket ... \n"
+
+PORT=8765
+exec 3<>/dev/tcp/localhost/$PORT
+# Check if the socket creation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to create TCP socket at port $PORT"
+  exit 1
+else
+  echo "Successfully created TCP socket at port $PORT"
+  exec 3>&-
+  exec 3<&-
+fi
+
 echo -e "\nEnvironment variable VERSION: ${VERSION} \n"
 echo -e "\nEnvironment variable SOME_ENV_VALUE: ${SOME_ENV_VALUE} \n"
 
