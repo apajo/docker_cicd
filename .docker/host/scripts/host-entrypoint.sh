b43c814fee738b7ead9e21e028c93e111ca2c1d0
Andres Pajo <andres@apajo.ee>
2024-08-11 00:52:54 +0300

    wip

--- .docker/host/scripts/host-entrypoint.sh
+++ .docker/host/scripts/host-entrypoint.sh
@@ -40,14 +40,11 @@ until docker info >/dev/null 2>&1; do
   sleep 1
 done
 
-# Start SSHD in the foreground
-exec /usr/sbin/sshd -D -e
-
 # Wait for staging server
 wait-for-it.sh ${STAGING_HOST}:${STAGING_PORT} -t 60;
 ssh-keyscan ${STAGING_HOST} >> /home/cicd/.ssh/known_hosts;
 
 echo -e "\Host is ready ... \n"
 
-
-# exec "$@"
+# Start SSHD in the foreground
+exec /usr/sbin/sshd -D -e
